678d2003d465e22bbe2ece3568778dde53fbe919
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-05-12 14:34:56 +0900

    fix: use posterior distribution
    Add num_lower_quantiles argument to BatchCenterGapQuantileGP

--- src/gpytorch_qr/gpqr_cg.py
+++ src/gpytorch_qr/gpqr_cg.py
@@ -31,7 +31,7 @@
     )
 
     class MyGP(BatchCenterGapQuantileGP):
-        def __init__(self, inducing_points, num_quantiles):
+        def __init__(self, inducing_points, num_quantiles, num_lower_quantiles):
             N, D = inducing_points.size()
             variational_distribution = CholeskyVariationalDistribution(
                 N,
@@ -52,11 +52,13 @@
                 RBFKernel(ard_num_dims=D, batch_shape=torch.Size([num_quantiles])),
                 batch_shape=torch.Size([num_quantiles]),
             )
-            super().__init__(variational_strategy, center_mean, gap_mean, covar)
+            super().__init__(
+                variational_strategy, center_mean, gap_mean, covar, num_lower_quantiles
+            )
 
     inducing_points = torch.linspace(0, 1, 10).reshape(-1, 1)
     central_q_index = (q - 0.5).abs().argmin().item()
-    gp = MyGP(inducing_points, len(q))
+    gp = MyGP(inducing_points, len(q), central_q_index)
     likelihood = BatchCenterGapALDLikelihood(q, central_q_index)
 
     from gpytorch.mlls import VariationalELBO
@@ -79,7 +81,7 @@
     gp.eval()
     x_pred = torch.linspace(0, 2, 100).reshape(-1, 1)
     with torch.no_grad():
-        quantiles = gp.mean_quantiles(x_pred, central_q_index)
+        quantiles = gp.mean_quantiles(x_pred)
 
     import matplotlib.pyplot as plt
     plt.scatter(x, y, c='gray', marker='.', alpha=0.1)
@@ -112,13 +114,23 @@ class BatchCenterGapQuantileGP(gpytorch.models.ApproximateGP):
         The mean module for the gaps between quantiles.
     covar_module : gpytorch.kernels.Kernel
         The covariance module for the Gaussian process.
+    num_lower_quantiles : int
+        The number of lower quantiles in center-gap representation.
     """
 
-    def __init__(self, variational_strategy, center_mean, gap_mean, covar_module):
+    def __init__(
+        self,
+        variational_strategy,
+        center_mean,
+        gap_mean,
+        covar_module,
+        num_lower_quantiles,
+    ):
         super().__init__(variational_strategy)
         self.center_mean = center_mean
         self.gap_mean = gap_mean
         self.covar_module = covar_module
+        self.num_lower_quantiles = num_lower_quantiles
 
     def forward(self, x):
         center_mean = self.center_mean(x)
@@ -127,32 +139,28 @@ class BatchCenterGapQuantileGP(gpytorch.models.ApproximateGP):
         covar = self.covar_module(x)
         return gpytorch.distributions.MultivariateNormal(mean, covar)
 
-    def joint_quantile_posterior(self, x, L):
+    def joint_quantile_posterior(self, x):
         """Joint posterior over quantiles at input locations.
 
         Parameters
         ----------
         x : torch.Tensor with shape (N, D)
             The input locations.
-        L : int
-            The number of lower quantiles in center-gap representation.
 
         Returns
         -------
         quantile_posterior : torch.distributions.TransformedDistribution
             Joint posterior over quantiles at input locations.
         """
-        return transform_centergap_posterior(self(x), L)
+        return transform_centergap_posterior(self(x), self.num_lower_quantiles)
 
-    def mean_quantiles(self, x, L, num_samples=10):
+    def mean_quantiles(self, x, num_samples=10):
         """Predict quantiles by MC mean of the quantile posterior.
 
         Parameters
         ----------
         x : torch.Tensor with shape (N, D)
             The input locations.
-        L : int
-            The number of lower quantiles in center-gap representation.
         num_samples : int, default=10
             Number of MC samples used to estimate the mean.
 
@@ -161,7 +169,7 @@ class BatchCenterGapQuantileGP(gpytorch.models.ApproximateGP):
         quantiles : torch.Tensor with shape (Q, N)
             The predicted quantiles at the input locations.
         """
-        dist = self.joint_quantile_posterior(x, L)
+        dist = self.joint_quantile_posterior(x)
         samples = dist.rsample(torch.Size([num_samples]))  # (num_samples, Q, N)
         return samples.mean(dim=0)  # (Q, N)
 
