9c7f0b966769925b1787c10c726a77c1b9df54c7
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-06-01 13:04:39 +0900

    feat: add more LMC structures
    Add CGLmcVariationalStrategy

--- src/gpytorch_qr/variational.py
+++ src/gpytorch_qr/variational.py
@@ -4,11 +4,12 @@ import gpytorch
 import torch
 
 __all__ = [
+    "CGLmcVariationalStrategy",
     "CGBlkdiagLmcVariationalStrategy",
 ]
 
 
-class CGBlkdiagLmcVariationalStrategy(gpytorch.variational.LMCVariationalStrategy):
+class CGLmcVariationalStrategy(gpytorch.variational.LMCVariationalStrategy):
     """LMC variational strategy for the center-gap quantile regression model.
 
     This class modifies the standard LMC coefficients to fit the center-gap
@@ -16,8 +17,7 @@ class CGBlkdiagLmcVariationalStrategy(gpytorch.variational.LMCVariationalStrateg
     The first latent function directly represents the central quantile, and it
     does not form any linear combinations with the other latent functions.
     The remaining latent functions are linearly combined to model the gap
-    functions between quantiles. Upper and lower gap functions are modeled
-    separately by block diagonal matrices.
+    functions between quantiles.
     """
 
     def __init__(
@@ -25,8 +25,6 @@ class CGBlkdiagLmcVariationalStrategy(gpytorch.variational.LMCVariationalStrateg
         base_variational_strategy,
         num_quantiles,  # Q
         num_latents,  # T
-        num_lower_quantiles,
-        num_lower_latents,
         latent_dim=-1,
         jitter_val=None,
     ):
@@ -41,21 +39,65 @@ class CGBlkdiagLmcVariationalStrategy(gpytorch.variational.LMCVariationalStrateg
         lmc_coefficients = self.lmc_coefficients.detach().clone()
         del self.lmc_coefficients
 
-        num_upper_quantiles = num_quantiles - num_lower_quantiles - 1
-        num_upper_latents = num_latents - num_lower_latents - 1
-
-        mask = torch.zeros_like(lmc_coefficients)
-        mask[..., 1 : 1 + num_lower_latents, 1 : 1 + num_lower_quantiles] = 1
-        mask[..., -num_upper_latents:, -num_upper_quantiles:] = 1
-        self.register_buffer("lmc_mask", mask)
-
         g0_mask = torch.zeros_like(lmc_coefficients)
         g0_mask[..., 0, 0] = 1
         self.register_buffer("g0_mask", g0_mask)
 
+        self.register_buffer(
+            "lmc_mask", self.construct_lmc_mask(lmc_coefficients.shape)
+        )
+
         self.register_parameter(
             "_lmc_coefficients", torch.nn.Parameter(lmc_coefficients)
         )
+
+    def construct_lmc_mask(self, shape):
+        mask = torch.zeros(shape)
+        mask[..., 1:, 1:] = 1
+        return mask
+
+    @property
+    def lmc_coefficients(self):
+        return self._lmc_coefficients * self.lmc_mask + self.g0_mask
+
+
+class CGBlkdiagLmcVariationalStrategy(CGLmcVariationalStrategy):
+    """LMC variational strategy for the center-gap quantile regression model.
+
+    Upper and lower gap functions are modeled separately by block diagonal matrices.
+    """
+
+    def __init__(
+        self,
+        base_variational_strategy,
+        num_quantiles,  # Q
+        num_latents,  # T
+        num_lower_quantiles,
+        num_lower_latents,
+        latent_dim=-1,
+        jitter_val=None,
+    ):
+        num_upper_quantiles = num_quantiles - num_lower_quantiles - 1
+        num_upper_latents = num_latents - num_lower_latents - 1
+
+        self.num_lower_quantiles = num_lower_quantiles
+        self.num_lower_latents = num_lower_latents
+        self.num_upper_quantiles = num_upper_quantiles
+        self.num_upper_latents = num_upper_latents
+
+        super().__init__(
+            base_variational_strategy,
+            num_quantiles,
+            num_latents,
+            latent_dim,
+            jitter_val,
+        )
+
+    def construct_lmc_mask(self, shape):
+        mask = super().construct_lmc_mask(shape)
+        mask[..., 1 : 1 + self.num_lower_latents, -self.num_upper_quantiles :] = 0
+        mask[..., -self.num_upper_latents :, 1 : 1 + self.num_lower_quantiles] = 0
+        return mask
 
     @property
     def lmc_coefficients(self):
