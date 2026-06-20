8addb971529263849dc909d99043a1492039f026
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-06-20 09:46:07 +0900

    feat: multi-output cg mtgpqr
    Redesign mean

--- src/gpytorch_qr/means.py
+++ src/gpytorch_qr/means.py
@@ -13,12 +13,15 @@ class CenterGapMean(Mean):
 
     Parameters
     ----------
-    center_mean : gpytorch.means.Mean
+    center_mean : gpytorch.means.Mean or torch.nn.ModuleList of gpytorch.means.Mean
         Mean module for the central quantile.
-        Should have batch shape ``(*B, 1)``.
+        If a ``torch.nn.ModuleList`` is provided, each of the contained mean modules
+        applies to a different output dimension.
+        Each mean should have batch shape ``(*B, 1)``.
     gap_mean : gpytorch.means.Mean
         Mean module for the quantile gaps.
-        Should have batch shape ``(*B, L-1)`` where *L* is the number of latent GPs.
+        Should have batch shape ``(*B, L-k)`` where *L* is the number of latent GPs
+        and *k* is the number of output dimensions.
 
     Notes
     -----
@@ -28,6 +31,8 @@ class CenterGapMean(Mean):
 
     def __init__(self, center_mean, gap_mean):
         super().__init__()
+        if not isinstance(center_mean, torch.nn.ModuleList):
+            center_mean = torch.nn.ModuleList([center_mean])
         self.center_mean = center_mean
         self.gap_mean = gap_mean
 
@@ -38,7 +43,13 @@ class CenterGapMean(Mean):
         ----------
         x : torch.Tensor in shape ``(*B, 1, N, D)``
             *N* is the number of data points and *D* is the number of input dimensions.
+
+        Returns
+        -------
+        torch.Tensor in shape ``(*B, L, N)``
         """
-        center_mean = self.center_mean(x)  # (*B, 1, N)
-        gap_mean = self.gap_mean(x)
+        center_mean = torch.concat(
+            [m(x) for m in self.center_mean], dim=-2
+        )  # (*B, k, N)
+        gap_mean = self.gap_mean(x)  # (*B, L-k, N)
         return torch.concat([center_mean, gap_mean], dim=-2)
