5762a5b0f296e226e1a08e0b49470f0fd895796e
Jisoo Song <jeesoo9595@snu.ac.kr>
2026-08-08 21:14:04 +0900

    maint: add doc publish check run

--- .github/k8s/dispatch-doc.sh
+++ .github/k8s/dispatch-doc.sh
@@ -2,6 +2,10 @@ if [ -z "${GITHUB_RELEASE_TAG_NAME:-}" ]; then
   echo "Missing required environment variable: GITHUB_RELEASE_TAG_NAME" >&2
   exit 1
 fi
+if [ -z "${PUBLISH_DOC_CHECK_RUN_ID:-}" ]; then
+  echo "Missing required environment variable: PUBLISH_DOC_CHECK_RUN_ID" >&2
+  exit 1
+fi
 
 source .github/k8s/app-token.sh
 
@@ -11,9 +15,12 @@ import sys
 
 print(json.dumps({
     "event_type": "build-release-docs",
-    "client_payload": {"release_tag": sys.argv[1]},
+    "client_payload": {
+        "release_tag": sys.argv[1],
+        "check_run_id": sys.argv[2],
+    },
 }))
-' "$GITHUB_RELEASE_TAG_NAME")"
+' "$GITHUB_RELEASE_TAG_NAME" "$PUBLISH_DOC_CHECK_RUN_ID")"
 
 curl --fail --silent --show-error --request POST \
   --header "Authorization: Bearer $installation_token" \
